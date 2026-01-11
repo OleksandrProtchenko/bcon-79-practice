602c5d5d58d1baef3be1f19df1ba07de324257bb
Ordinateur 1 <abcvika3@gmail.com>
2026-01-11 20:14:03 +0100

    add final

--- module-01/lesson-01/script.js
+++ module-01/lesson-01/script.js
@@ -1,72 +1,81 @@
+//1
 const myName = "Viktoria";
 console.log(myName);
 
+//2
 const birthMonth = 6;
 console.log(birthMonth);
 
-
+//3
 const str = " Ласкаво " + "просимо " + "на курс";
 console.log(str);
 
-// alert(2019);
+//4  alert(2019);
 
+//5
 const operation = 2019 - 200;
 console.log(operation);
 
-//5
-
+//6
 const containerEl = document.getElementById("six");
 console.log(containerEl);
 containerEl.textContent = "Hello World";
 
-//6
-const wrapperEl = document.getElementByld("seven");
+//7
+const wrapperEl = document.getElementById("seven");
 console.log(wrapperEl);
 wrapperEl.textContent = 12 * 12;
 
-//7
+//8
 const out8 = document.querySelector(".out-8");
 console.log(out8);
 out8.textContent = "task-8";
 
+//9
 const out9 = document.querySelector(".out-8");
 console.log(out9);
+out9.textContent += "World";
+//out9.texyContent = ""; 
 
-const elementOut10 = document.querySelector(out10);
-console.log(data[0] elementOut10, data[1]elementOut10);
-elementOut10.innerHTML = "<h2>Hi</h2>";
+//10
+const out10 = document.querySelector(".out-10");
+console.log(out10);
+out10.innerHTML = '&#x3C;h2&#x3E;Hi&#x3C;/h2&#x3E;';
 // elementOut10.textContent = "<h2>Hi</h2>";
 
 //11
-
-const elementOut11 = document.querySelector(".out-11");
-console.log(data[0] elementOut11, data[1]elementOut11);
-elementOut11.innerHTML = "123";
-elementOut11.innerHTML += "456";
+const out11 = document.querySelector(".out-11");
+console.log(out11);
+out11.innerHTML = "123";
+out11.innerHTML += "456";
+//out11.innerHTML = out11.innerHTML + "456";
 
 //12
-const a = document.querySelector(.out-12);
-console.log( data0 a , data1 a);
+const a = document.querySelector(".out-12");
+console.log(a);
 a.innerHTML = 3.1415;
 // a.innerHTML = Math.ceil(3.1415);
 
-const out-13 = documentSelector(".out-13");
-console.log(data 0 out13, data1 out13);
-out13.innerHTML = "https://image.unsplash.com/photo-1579468118864   alt-js width"
+
+//13
+const out13 = document.querySelector(".out-13");
+console.log(out13);
+out13.innerHTML = 
+'<img src="https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a" alt="js" width="400">'
+
 
 //14
-
 const z1 = 6;
 const z2 = 3;
-const out14 = document.querySelector(".out14");
 const multiplication = z1 * z2;
 console.log(multiplication);
+const out14 = document.querySelector(".out-14");
 out14.innerHTML = multiplication;
 
 //15
 const y1 = 6;
 const y2 = 3;
-const out15 = document.querySelector("out15");
+const out15 = document.querySelector(".out-15");
 const divition = y1 / y2;
 console.log(divition);
 out15.innerHTML = divition;
@@ -74,25 +83,28 @@ out15.innerHTML = divition;
 //16
 const x1 = "Hello";
 const x2 = 5;
-const out16 = document.querySelector(".out16");
+const out16 = document.querySelector(".out-16");
 const summ = x1 + x2;
 console.log(summ);
 out16.innerHTML = summ;
 
 //17
-const out17 = document.querySelector(".out17");
+const out17 = document.querySelector(".out-17");
 console.dir(out17);
+//виводить в console як об'єкт і всі його дані як об'єкта джави
 
 //18
-let out18 = document.querySelector("out18");
+let out18 = document.querySelector(".out-18");
 out18 = 5;
 console.log(out18);
 
 //19
-let out19 = document.querySelector(".out19");
+let out19 = document.querySelector(".out-19");
 console.log(out19);
-out19 = document.querySelector(".out19-test");
+out19 = document.querySelector(".out-19-test");
 console.log(out19);
 
 //20
-let out20 = 
+const out20 = document.querySelector(".out-20");
+out20.textContent = "&#x27;&#x3C;h2&#x3E;Hi&#x3C;/h2&#x3E;&#x27;";
+console.log(out20);
--- module-01/lesson-02/script.js
+++ module-01/lesson-02/script.js
@@ -1,10 +1,12 @@
 //1
-
-
-
-
+a = 7;
+b = 9;
+console.log(a * b);
 
 //2
+c = 7;
+d = 9;
+out2 = c / 9;
 
 
 
@@ -20,44 +22,44 @@
 //4
 
 
-//5
-//6
-//7
-const e4 = 3;
-const f4 = "hello";
-const out7 = document.querySelector(".out7");
-console.log("out7:", "out7")
+// //5
+// //6
+// //7
+// const e4 = 3;
+// const f4 = "hello";
+// const out7 = document.querySelector(".out7");
+// console.log("out7:", "out7")
 
 
 
-//8
+// //8
+// // function fn8() {
+// // console.log("work");
+// // }
+// const byn8 = document.querySelector(".b-8");
+// const byn8 = document.querySelector(".i-8");
+// const byn8 = document.querySelector(".out-8");
+
+// btn8.onclick = fn8;
+
 // function fn8() {
-// console.log("work");
+// //const userInput = input8.value;
+// //console.log("userInput:", userInput);
+//   out8.textContent = input.value;
+//   input.value = "";     //записали пустий рядок щоб витерти//
 // }
-const byn8 = document.querySelector(".b-8");
-const byn8 = document.querySelector(".i-8");
-const byn8 = document.querySelector(".out-8");
 
-btn8.onclick = fn8;
+// //9
+// const byn8 = document.querySelector(".b-9");
+// const byn8 = document.querySelector(".i-9");
+// const byn8 = document.querySelector(".out-9");
 
-function fn8() {
-//const userInput = input8.value;
-//console.log("userInput:", userInput);
-  out8.textContent = input.value;
-  input.value = "";     //записали пустий рядок щоб витерти//
-}
+// btn9.onclick = t9;
 
-//9
-const byn8 = document.querySelector(".b-9");
-const byn8 = document.querySelector(".i-9");
-const byn8 = document.querySelector(".out-9");
+// function t9() {
 
-btn9.onclick = t9;
-
-function t9() {
-
-  out9.textContent = input.value;
-  input.value = ""; 
+//   out9.textContent = input.value;
+//   input.value = ""; 
 
 
-}
+// }
